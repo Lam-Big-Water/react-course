b713461b1c0b2bae73a61d5197850d85090af3c1
Sam <xlin67948@gmail.com>
2023-11-01 09:42:23 +0800

    step-1: done

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Wal',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+
+      <hr />
+
+      <List />
+    </div>
+  );
+}
+
+const List = () => {
+  return (
+    <ul>
+      {list.map((item, index) => {
+        return (
+          <li key={item.objectID}>
+            {/* only use an index as last resort */}
+            {/* and by the way: that's how you do comments in JSX */}
+            {item.title}
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
-export default App
+const Search = () => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id='search' type="text" />
+    </div>
+  )
+}
+
+export default App; 
--- hacker-stories/vite.config.js
+++ hacker-stories/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// # leanpub-start-insert
+import eslint from 'vite-plugin-eslint';
+// # leanpub-end-insert
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+// # leanpub-start-insert
+  plugins: [react(), eslint()],
+// # leanpub-end-insert
+});
