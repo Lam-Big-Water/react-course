cc11351d070001779ab54e568a1c48f4f142f648
Sam <xlin67948@gmail.com>
2023-11-15 20:09:57 +0800

    Props-Handling(Ad)

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -47,34 +47,43 @@ const App = () => {
   );
 }
 
-const Search = (props) => (
+const Search = ({search, onSearch}) => {
 
-      <div>
+  // * destructuring
+  // const {search, onSearch} = props;
+
+  return (
+    <div>
         <label htmlFor="search">Search: </label>
         
-        <input id='search' type="text" value={props.search} onChange={props.onSearch}/>
+        <input id='search' type="text" value={search} onChange={onSearch}/>
       </div>
-);
+  );
+};
 
-const List = (props) => (
+const List = ({list}) => (
     <ul>
       {/* Distribute data */}
-      {props.list.map((item) => (
-        <Item key={item.objectID} item={item}/>
+      {list.map(({objectID, ...item}) => (
+        <Item key={objectID} {...item}/>
       ))}
     </ul>
 );
 
-const Item = (props) => (
-  <li>
-    <span>
-      <a href={props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
-);
+const Item = ({title, url, author, num_comments, points}) => {
+
+    // * nested destructuring
+    return (
+      <li>
+        <span>
+          <a href={url}>{title}</a>
+        </span>
+        <span>{author}</span>
+        <span>{num_comments}</span>
+        <span>{points}</span>
+      </li>
+    );
+};
 
 
 
